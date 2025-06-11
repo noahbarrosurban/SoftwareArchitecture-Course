0a35e05f5acfac08194586d4e8660d8b4d1c1c32
Noah Barros Urban <noahbarrosurban@gmail.com>
2025-06-11 19:54:07 -0300

    feat: adiciona pipeline

--- src/repositories/equipmentRepository.js
+++ src/repositories/equipmentRepository.js
@@ -1,4 +1,4 @@
-import { BaseRepository } from "./BaseRepository.js";
+import { BaseRepository } from "./baseRepository.js";
 import { equipment } from "../models/entity/equipment.js";
 
 export class EquipmentRepository extends BaseRepository {
